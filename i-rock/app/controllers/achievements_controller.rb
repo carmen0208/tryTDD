04dec22eda387fb4c7ad82a10b83d2b0ff39f935
Carmen Liu <carmen.liujia@gmail.com>
2016-12-18 10:17:10 +1300

    factory girl

--- i-rock/app/controllers/achievements_controller.rb
+++ i-rock/app/controllers/achievements_controller.rb
@@ -11,6 +11,10 @@ class AchievementsController < ApplicationController
     end
   end
 
+  def show
+    @achievement = Achievement.find(params[:id])
+    @description = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@achievement.description)
+  end
   private
   def achievement_params
     params.require(:achievement).permit(:title,:description,:privacy, :cover_image,:featured)
---
+++ i-rock/spec/fixtures/achievement_page_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'achievement page' do
+  scenario 'achievement public page' do
+    # achievement = Achievement.create(title: 'Just did it')
+    achievement = FactoryGirl.create(:achievement, title: 'Just did it')
+    visit("/achievements/#{achievement.id}")
+
+    expect(page).to have_content('Just did it')
+
+    # achievements = FactoryGirl.create_list(:achievement,3)
+    # p achievements
+  end
+
+  scenario 'render markdown description' do
+    # it is hard because title can't be blank even if we only want to test is description
+    achievement = FactoryGirl.create(:achievement, description: 'That **was** hard')
+    visit("/achievements/#{achievement.id}")
+
+    # expect(page).to have_content('<strong>was</strong>')
+    expect(page).to have_css('strong',text: "was")
+
+  end
+end
