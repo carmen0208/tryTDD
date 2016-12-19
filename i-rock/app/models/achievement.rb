23a5cb1e64745f65b3ea1d35ade552daff3d434f
Carmen Liu <carmen.liujia@gmail.com>
2016-12-19 19:42:13 +1300

    how to test model with SQL Query

--- i-rock/app/models/achievement.rb
+++ i-rock/app/models/achievement.rb
@@ -15,11 +15,20 @@ class Achievement < ActiveRecord::Base
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
   end
 
-  private
+  def silly_title
+    "#{title} by #{user.email}"
+  end
+
+  def self.by_letter(letter)
+    includes(:user).where("title LIKE ?", "#{letter}%").order("users.email")
+  end
+  # private
   # def unique_title_for_one_user
   #   existing_achievement = Achievement.find_by(title: title)
   #   if (existing_achievement && existing_achievement.user == user)
   #     errors.add(:title, "you can't have two achievements with the same title")
   #   end
   # end
+
+
 end
--- i-rock/spec/models/achievement_spec.rb
+++ i-rock/spec/models/achievement_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe Achievement, type: :model do
     # end
     it { should validate_uniqueness_of(:title).scoped_to(:user_id).with_message("you can't have two achievements with the same title")}
   end
-  # 
+  #
   # it 'belongs to user' do
   #   achievement = Achievement.new(title: 'Some Title', user: nil)
   #   expect(achievement.valid?).to be_falsy
@@ -49,4 +49,33 @@ RSpec.describe Achievement, type: :model do
   # end
 
   it { should belong_to(:user) }
+
+  it 'converts markdown to html' do
+    achievement = Achievement.new(description: 'Awesome **thing** I *actually* did')
+    expect(achievement.description_html).to include('<strong>thing</strong>')
+    expect(achievement.description_html).to include('<em>actually</em>')
+  end
+
+  it 'has silly title' do
+    achievement = Achievement.new(title: "New Achievement", user: FactoryGirl.create(:user, email:'test@test.com'))
+    expect(achievement.silly_title).to eq("New Achievement by test@test.com")
+  end
+
+  it 'only fetches achievements which title starts from provided letter' do
+    user = FactoryGirl.create(:user)
+    achievement1 = FactoryGirl.create(:public_achievement, title: 'Read a book', user: user)
+    achievement2 = FactoryGirl.create(:public_achievement, title: 'Passed an exam', user: user)
+    expect(Achievement.by_letter("R")).to eq([achievement1])
+  end
+
+  it "sorts achievements by user emails" do
+    albert = FactoryGirl.create(:user, email: 'albert@email.com')
+    rob = FactoryGirl.create(:user, email: 'rob@email.com')
+    achievement1 = FactoryGirl.create(:public_achievement, title: 'Read a book', user: rob)
+    achievement2 = FactoryGirl.create(:public_achievement, title: 'Rocket it', user: albert)
+    expect(Achievement.by_letter("R")).to eq([achievement2, achievement1])
+  end
+
+
+
 end
